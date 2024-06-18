d92f0d69e5e4d2aed43f71111c303d1988c8c13d
Mohdhussain28 <mohdhussain28@outlook.com>
2024-06-18 18:03:01 +0530

    a

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,9 @@
 const admin = require("firebase-admin");
-admin.initializeApp();
+
+var serviceAccount = require("./serviceAccount1.json");
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
 
 module.exports = {
     v1: require("./v1"),
